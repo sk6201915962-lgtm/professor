1963a409e2b57897e3d375ae3cfc39a9310203e7
sk6201915962-lgtm <sk6201915962@gmail.com>
2025-09-24 03:02:17 +0530

    updated website

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Dr. Soumya Ranjan Nayak',
+  description: 'Portfolio',
   generator: 'v0.app',
 }
 
